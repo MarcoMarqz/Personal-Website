57362f18df3df370ec6c3c256815f575ad20fe02
Marco Marquez <117479973+MarcoMarqz@users.noreply.github.com>
2026-05-20 20:19:58 -0400

    Html/css/js desktop format design base

--- READ.md
+++ READ.md
@@ -1,13 +1,3 @@
-# Notes for Loops Section
-
-
-```javascript
-let num = 42;
-console.log(num.toString());
-console.log(`${num}`);
-console.log('' +num);
-console.log(String(num));
-```
 
 
 
--- js/main.js
+++ js/main.js
@@ -10,9 +10,45 @@ function showPanel(event){
       btn.setAttribute("aria-selected", "false")
     }
   );
+
+  document.querySelectorAll("button").forEach(
+    (btn) =>{
+      if(btn.id == event.target.id){
+        btn.setAttribute("aria-selected" , true);
+      }
+    }
+  );
+
+
+  document.querySelectorAll('[role = "tabpanel"]').forEach( 
+    (tab) =>{
+      tab.setAttribute('hidden', 'true');
+  });
+
+
+    document.querySelectorAll('[role = "tabpanel"]').forEach( 
+    (panel) =>{
+      
+      if(panel.getAttribute("aria-labelledby") == currentTab.id){
+        panel.removeAttribute('hidden');
+
+
+      }
+  });
+
+
 }
 
 
 document.querySelectorAll("button").forEach( (btn) => {
   btn.addEventListener('click', showPanel);
 });
+
+
+const appIcon = document.querySelector("#app-icon");
+const windowBox = document.querySelector("#window");
+
+appIcon.addEventListener("click", () => {
+    windowBox.hidden = false;
+});
+
